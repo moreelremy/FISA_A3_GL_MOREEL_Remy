38707174e5a99e1afb9aaa6dbb87ca59c4463387
Axel <axel09.delar@gmail.com>
2025-02-04 16:05:11 +0100

    fix: .gitignore

--- EasySave/EasySave/obj/Debug/net8.0/EasySave.AssemblyInfo.cs
+++ EasySave/EasySave/obj/Debug/net8.0/EasySave.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EasySave")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed215b01601d220b0b48184c0f5695cf465e4d7b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5aeb6d316c61893a612ca1fca5f74e5ddf1e447c")]
 [assembly: System.Reflection.AssemblyProductAttribute("EasySave")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EasySave")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
