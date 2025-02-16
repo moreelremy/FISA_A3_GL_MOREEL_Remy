66d5bfa7cfa3bf24ce55707b13c8c428b09ab7bf
KisaPulga <leoschaeffer03@gmail.com>
2025-02-16 21:14:04 +0100

    feature_language : changement de langue globale sur l'ensemble des pages

--- EasySave/EasySaveGUI/ViewModel/ShowLogs.xaml.cs
+++ EasySave/EasySaveGUI/ViewModel/ShowLogs.xaml.cs
@@ -1,5 +1,7 @@
 ﻿
+using EasySaveGUI.Helpers;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace EasySaveGUI
 {
@@ -11,6 +13,45 @@ namespace EasySaveGUI
         public ShowLogs()
         {
             InitializeComponent();
+            InitializeComponent();
+            DataContext = LanguageHelper.Instance;
+
+            // Charger la langue depuis les paramètres
+            string savedLanguage = Properties.Settings.Default.Language;
+
+            if (!string.IsNullOrEmpty(savedLanguage))
+            {
+                LanguageHelper.ChangeLanguage(savedLanguage);
+            }
+
+            // Sélectionner la bonne langue dans le ComboBox
+            var comboBox = FindName("LanguageComboBox") as ComboBox;
+            if (comboBox != null)
+            {
+                foreach (ComboBoxItem item in comboBox.Items)
+                {
+                    if (item.Tag != null && item.Tag.ToString() == savedLanguage)
+                    {
+                        item.IsSelected = true;
+                        break;
+                    }
+                }
+            }
+        }
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (sender is ComboBox comboBox && comboBox.SelectedItem is ComboBoxItem selectedItem)
+            {
+                string selectedLanguage = selectedItem.Tag.ToString();
+
+                // Sauvegarde dans les settings
+                Properties.Settings.Default.Language = selectedLanguage;
+                Properties.Settings.Default.Save();
+                Properties.Settings.Default.Reload();
+
+                // Change la langue immédiatement
+                LanguageHelper.ChangeLanguage(selectedLanguage);
+            }
         }
 
         private void ButtonLogsMenuClick(object sender, RoutedEventArgs e)
