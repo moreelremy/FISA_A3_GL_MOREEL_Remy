397f4a6468122baf0881dcca0d17d922220da988
KisaPulga <leoschaeffer03@gmail.com>
2025-02-17 16:17:55 +0100

    add-wpf-cryptosoft : ajout du projet WPF pour le crypto soft

---
+++ CryptoSoftWPF/CryptoSoftWPF/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace CryptoSoftWPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
