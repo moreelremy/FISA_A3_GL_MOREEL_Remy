6a5c5d3abfe06c2bf4ace3b9c53b35ecfb31a5be
Remy Moreel <moreelremy@gmail.com>
2025-02-18 09:16:14 +0100

    Version final de la bibliothèque Logger

--- EasySaveLogger/obj/Release/net8.0/EasySaveLogger.AssemblyInfo.cs
+++ EasySaveLogger/obj/Release/net8.0/EasySaveLogger.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EasySaveLogger")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8daed76655daf5ecbfbcefc2c96fad54a16381c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb9736334b162808862724f7d47929c64704eff6")]
 [assembly: System.Reflection.AssemblyProductAttribute("EasySaveLogger")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EasySaveLogger")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
