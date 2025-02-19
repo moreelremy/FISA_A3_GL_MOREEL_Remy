30b7a276989e04517332a8d918809ef8d6331436
gitygit362 <axel09.delar@gmail.com>
2025-02-19 21:32:08 +0100

    feature-chooseExtension: ajout de la logique pour spécifier une extension à crypter

--- EasySave/EasySaveGUI/ViewModel/ExecuteSavesViewModel.cs
+++ EasySave/EasySaveGUI/ViewModel/ExecuteSavesViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.ObjectModel;
+using System.Windows;
 using System.Windows.Input;
 
 namespace EasySaveGUI.ViewModel
@@ -11,6 +12,27 @@ namespace EasySaveGUI.ViewModel
 
         public ICommand ExecuteGlobalSaveCommand { get; }
         public ICommand ExecutePartialSaveCommand { get; }
+        public ICommand ChooseExtensionCommand { get; }
+
+        private List<string> _extensions;
+        private string _inputExtensions;
+
+
+        public List<string> Extensions
+        {
+            get => _extensions;
+            set
+            {
+                _extensions = value;
+            }
+        }
+
+
+        public string InputExtensions
+        {
+            get => _inputExtensions;
+            set { _inputExtensions = value; OnPropertyChanged(); }
+        }
 
         public ExecuteSavesViewModel()
         {
@@ -20,9 +42,30 @@ namespace EasySaveGUI.ViewModel
         public ExecuteSavesViewModel(SaveRepository saveRepository)
         {
             Saves = new ObservableCollection<Save>(saveRepository.GetAllSaves());
-            
+            ChooseExtensionCommand = new RelayCommand(_ => ChooseExtension());
+
+
         }
 
+
+        private void ChooseExtension()
+        {
+            string extensionsEntry = InputExtensions;
+            if (string.IsNullOrWhiteSpace(extensionsEntry))
+            {
+                MessageBox.Show("Veuillez entrer des extensions.");
+                return;
+            }
+
+
+            Extensions = extensionsEntry.Split(';', StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            foreach (var ext in Extensions)
+            {
+                MessageBox.Show(ext);
+            }
+
+        }
 
     }
 
