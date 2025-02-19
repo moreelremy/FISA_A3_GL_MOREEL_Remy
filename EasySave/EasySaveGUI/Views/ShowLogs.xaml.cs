ebf9284e1448c1568a2aeb553494e22727d9d164
KisaPulga <55848121+KisaPulga@users.noreply.github.com>
2025-02-19 23:01:36 +0100

    Revert "V2.7.4"

--- EasySave/EasySaveGUI/Views/ShowLogs.xaml.cs
+++ EasySave/EasySaveGUI/Views/ShowLogs.xaml.cs
@@ -7,7 +7,6 @@ using System.Diagnostics;
 using System.IO;
 using static Logs;
 using System.Text.RegularExpressions;
-using System.Windows.Media;
 
 namespace EasySaveGUI
 {
@@ -28,43 +27,6 @@ namespace EasySaveGUI
 
             objViewModel.LogsSearch();
 
-            InitializePlaceholders();
-
         }
-
-        private void InitializePlaceholders()
-        {
-            SetPlaceholder(InputLogsSearch, "WPF_EnterDate");
-        }
-
-        private void SetPlaceholder(TextBox textBox, string translationKey)
-        {
-            if (string.IsNullOrWhiteSpace(textBox.Text))
-            {
-                textBox.Text = LanguageHelper.Translate(translationKey);
-                textBox.Foreground = Brushes.Gray;
-            }
-        }
-
-        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
-        {
-            TextBox textBox = sender as TextBox;
-            if (textBox != null && textBox.Text == LanguageHelper.Translate("WPF_EnterDate"))
-            {
-                textBox.Text = "";
-                textBox.Foreground = Brushes.Black;
-            }
-        }
-
-        private void TextBox_LostFocus(object sender, RoutedEventArgs e)
-        {
-            TextBox textBox = sender as TextBox;
-            if (textBox != null && string.IsNullOrWhiteSpace(textBox.Text))
-            {
-                textBox.Text = LanguageHelper.Translate("WPF_EnterDate");
-                textBox.Foreground = Brushes.Gray;
-            }
-        }
-
     }
 }
