e5bd5cd8fde094ef8d0c4fb3a0fe20c22826c46d
Remy Moreel <moreelremy@gmail.com>
2025-02-20 16:31:07 +0100

    Factorisation de la deuxième méthode dans save

--- EasySave/EasySave.Shared/Model/Save.cs
+++ EasySave/EasySave.Shared/Model/Save.cs
@@ -37,7 +37,7 @@ public abstract class SaveStrategy
         string target = @"\" + save.name + @"\" + DateTime.Now.ToString("dd-MM-yyyy HH-mm-ss.ff");
         DateTime startSave = DateTime.UtcNow;
         int encryptionTime;
-        encryptionTime = SaveDirectory(save.sourceDirectory, string.Concat(save.targetDirectory, target), save.name, totalFilesToCopy, totalFileSize, totalFilesToCopy, totalFileSize, save.logFileExtension, 0);
+        encryptionTime = commonSaveDirectory(save.sourceDirectory, string.Concat(save.targetDirectory, target), save.name, totalFilesToCopy, totalFileSize, totalFilesToCopy, totalFileSize, save.logFileExtension, 0);
         // Log the end of the save in the real-time log
         Logs.RealTimeLog(
             saveName: save.name,
@@ -64,10 +64,71 @@ public abstract class SaveStrategy
     /// <param name="save">The save instance containing backup details.</param>
     public abstract void Save(Save save);
 
+    public int commonSaveDirectory(string sourceDirectory, string targetDirectory, string saveName, int totalFilesToCopy, long totalFileSize, int nbFilesLeftToDo, long filesSizeLeftToDo, string logFileExtension, int encryptionTime, DateTime? lastChangeDateTime = null)
+    {
+        if (!Directory.Exists(targetDirectory))
+        {
+            Directory.CreateDirectory(targetDirectory);
+        }
+
+        // TO MODIFY : avoid creating a directory if the calculator is launched from the start
+        // ADD : reading the settings json file for the name of the business software
+        string calculatorProcessName = "CalculatorApp";
+        var processes = Process.GetProcesses();
+        // Copy all files in the source directory to the target directory
+        foreach (string file in Directory.GetFiles(sourceDirectory))
+        {
+            bool isCalculatorRunning = processes.Any(p => p.ProcessName.ToLower() == calculatorProcessName.ToLower());
+            if (!isCalculatorRunning)
+            {
+                string target = Path.Combine(targetDirectory, Path.GetFileName(file));
+                // Copy the file if it has been modified since the last save
+                if (lastChangeDateTime == null || File.GetLastWriteTime(file) > lastChangeDateTime)
+                {
+                    File.Copy(file, target, true);
+                    if (true)// to replace 
+                    {
+                        DateTime startFilencryption = DateTime.UtcNow;
+                        Crypt.Encrypt(target, "02e5d449168bb31da11145d04d6da992ffc7f8f20c04dcf5a046f7620ee6236");
+                        DateTime stopFilencryption = DateTime.UtcNow;
+                        encryptionTime += (int)(stopFilencryption - startFilencryption).TotalMilliseconds;
+                    }
+                    long fileSize = new FileInfo(file).Length;
+                    nbFilesLeftToDo -= 1;
+                    filesSizeLeftToDo -= fileSize;
+                    // Log the file copy in the real-time log
+                    Logs.RealTimeLog(
+                        saveName: saveName,
+                        sourcePath: file,
+                        targetPath: target,
+                        fileSize: fileSize,
+                        state: "ACTIVE",
+                        totalFilesToCopy: totalFilesToCopy,
+                        totalFileSize: totalFileSize,
+                        nbFilesLeftToDo: nbFilesLeftToDo,
+                        filesSizeLeftToDo: filesSizeLeftToDo,
+                        Progression: (int)(((float)totalFileSize - (float)filesSizeLeftToDo) / (float)totalFileSize * 100),
+                        logFileExtension: logFileExtension
+                    );
+                }
+            }
+        }
+
+        // Copy all subdirectories in the source directory to the target directory
+        foreach (string directory in Directory.GetDirectories(sourceDirectory))
+        {
+            string target = Path.Combine(targetDirectory, Path.GetFileName(directory));
+            encryptionTime = commonSaveDirectory(directory, target, saveName, totalFilesToCopy, totalFileSize, nbFilesLeftToDo, filesSizeLeftToDo, logFileExtension, encryptionTime, lastChangeDateTime);
+            (nbFilesLeftToDo, filesSizeLeftToDo) = SaveDirectory(target, nbFilesLeftToDo, filesSizeLeftToDo, lastChangeDateTime);
+        }
+
+        return encryptionTime;
+    }
+
     /// <summary>
     /// Saves a directory by copying files and subdirectories.
     /// </summary>
-    public abstract int SaveDirectory(string sourceDirectory, string targetDirectory, string saveName, int totalFilesToCopy, long totalFileSize, int nbFilesLeftToDo, long filesSizeLeftToDo, string logFileExtension, int encryptionTime, DateTime? lastChangeDateTime = null);
+    public abstract (int,long) SaveDirectory(string target, int nbFilesLeftToDo, long filesSizeLeftToDo, DateTime? lastChangeDateTime = null);
 }
  
 /// <summary>
@@ -97,61 +158,11 @@ public class FullSave : SaveStrategy
     }
 
     /// <inheritdoc/>
-    public override int SaveDirectory(string sourceDirectory, string targetDirectory, string saveName, int totalFilesToCopy, long totalFileSize, int nbFilesLeftToDo, long filesSizeLeftToDo, string logFileExtension, int encryptionTime, DateTime? lastChangeDateTime = null)
+    public override (int, long) SaveDirectory(string target, int nbFilesLeftToDo, long filesSizeLeftToDo, DateTime? lastChangeDateTime = null)
     {
-        if (!Directory.Exists(targetDirectory))
-        {
-            Directory.CreateDirectory(targetDirectory);
-        }
-
-        // TO MODIFY : avoid creating a directory if the calculator is launched from the start
-        // ADD : reading the settings json file for the name of the business software
-        string calculatorProcessName = "CalculatorApp";
-        var processes = Process.GetProcesses();
-        // Copy all files in the source directory to the target directory
-        foreach (string file in Directory.GetFiles(sourceDirectory))
-        {
-            bool isCalculatorRunning = processes.Any(p => p.ProcessName.ToLower() == calculatorProcessName.ToLower());
-            if (!isCalculatorRunning)
-            {
-                string target = Path.Combine(targetDirectory, Path.GetFileName(file));
-                File.Copy(file, target, true);
-                if (true)// to replace 
-                {
-                    DateTime startFilencryption = DateTime.UtcNow;
-                    Crypt.Encrypt(target, "b6b1f3a002ee5ef1856b716e06bf3298b6e94fe15471098edc3a4278b9e35d3");
-                    DateTime stopFilencryption = DateTime.UtcNow;
-                    encryptionTime += (int)(stopFilencryption - startFilencryption).TotalMilliseconds;
-                }
-                long fileSize = new FileInfo(file).Length;
-                nbFilesLeftToDo -= 1;
-                filesSizeLeftToDo -= fileSize;
-                // Log the file copy in the real-time log
-                Logs.RealTimeLog(
-                    saveName: saveName,
-                    sourcePath: file,
-                    targetPath: target,
-                    fileSize: fileSize,
-                    state: "ACTIVE",
-                    totalFilesToCopy: totalFilesToCopy,
-                    totalFileSize: totalFileSize,
-                    nbFilesLeftToDo: nbFilesLeftToDo,
-                    filesSizeLeftToDo: filesSizeLeftToDo,
-                    Progression: (int)(((float)totalFileSize - (float)filesSizeLeftToDo) / (float)totalFileSize * 100),
-                    logFileExtension: logFileExtension
-                );
-            }
-        }
-
-        // Copy all subdirectories in the source directory to the target directory
-        foreach (string directory in Directory.GetDirectories(sourceDirectory))
-        {
-            string target = Path.Combine(targetDirectory, Path.GetFileName(directory));
-            encryptionTime = SaveDirectory(directory, target, saveName, totalFilesToCopy, totalFileSize, nbFilesLeftToDo, filesSizeLeftToDo, logFileExtension, encryptionTime);
-            nbFilesLeftToDo -= Directory.GetFiles(target, "*.*", SearchOption.AllDirectories).Count();
-            filesSizeLeftToDo -= Directory.GetFiles(target, "*.*", SearchOption.AllDirectories).Sum(file => new FileInfo(file).Length);
-        }
-        return encryptionTime;
+        nbFilesLeftToDo -= Directory.GetFiles(target, "*.*", SearchOption.AllDirectories).Count();
+        filesSizeLeftToDo -= Directory.GetFiles(target, "*.*", SearchOption.AllDirectories).Sum(file => new FileInfo(file).Length);
+        return (nbFilesLeftToDo, filesSizeLeftToDo);
     }
 }
 
@@ -189,59 +200,11 @@ public class DifferentialSave : SaveStrategy
     }
 
     /// <inheritdoc/>
-    public override int SaveDirectory(string sourceDirectory, string targetDirectory, string saveName, int totalFilesToCopy, long totalFileSize, int nbFilesLeftToDo, long filesSizeLeftToDo, string logFileExtension, int encryptionTime, DateTime? lastChangeDateTime = null)
+    public override (int, long) SaveDirectory(string target, int nbFilesLeftToDo, long filesSizeLeftToDo, DateTime? lastChangeDateTime = null)
     {
-        if (!Directory.Exists(targetDirectory))
-        {
-            Directory.CreateDirectory(targetDirectory);
-        }
-
-        // Copy all files in the source directory to the target directory
-        foreach (string file in Directory.GetFiles(sourceDirectory))
-        {
-            string target = Path.Combine(targetDirectory, Path.GetFileName(file));
-
-            // Copy the file if it has been modified since the last save
-            if (lastChangeDateTime == null || File.GetLastWriteTime(file) > lastChangeDateTime)
-            {
-                File.Copy(file, target, true);
-                if (true)// to replace 
-                {
-                    DateTime startFilencryption = DateTime.UtcNow;
-                    Crypt.Encrypt(target, "02e5d449168bb31da11145d04d6da992ffc7f8f20c04dcf5a046f7620ee6236");
-                    DateTime stopFilencryption = DateTime.UtcNow;
-                    encryptionTime += (int)(stopFilencryption - startFilencryption).TotalMilliseconds;
-                }
-                long fileSize = new FileInfo(file).Length;
-                nbFilesLeftToDo -= 1;
-                filesSizeLeftToDo -= fileSize;
-                // Log the file copy in the real-time log
-                Logs.RealTimeLog(
-                    saveName: saveName,
-                    sourcePath: file,
-                    targetPath: target,
-                    fileSize: fileSize,
-                    state: "ACTIVE",
-                    totalFilesToCopy: totalFilesToCopy,
-                    totalFileSize: totalFileSize,
-                    nbFilesLeftToDo: nbFilesLeftToDo,
-                    filesSizeLeftToDo: filesSizeLeftToDo,
-                    Progression: (int)(((float)totalFileSize - (float)filesSizeLeftToDo) / (float)totalFileSize * 100),
-                    logFileExtension: logFileExtension
-                );
-            }
-        }
-
-        // Copy all subdirectories in the source directory to the target directory
-        foreach (string directory in Directory.GetDirectories(sourceDirectory))
-        {
-            string target = Path.Combine(targetDirectory, Path.GetFileName(directory));
-            encryptionTime = SaveDirectory(directory, target, saveName, totalFilesToCopy, totalFileSize, nbFilesLeftToDo, filesSizeLeftToDo, logFileExtension, encryptionTime, lastChangeDateTime);
-            // Count the number of files to copy and the total size of the files
-            string[] filesToCopy = Directory.GetFiles(target, "*.*", SearchOption.AllDirectories);
-            nbFilesLeftToDo -= filesToCopy.Where(file => lastChangeDateTime == null || File.GetLastWriteTime(file) > lastChangeDateTime).Count();
-            filesSizeLeftToDo -= filesToCopy.Where(file => lastChangeDateTime == null || File.GetLastWriteTime(file) > lastChangeDateTime).Sum(file => new FileInfo(file).Length);
-        }
-        return encryptionTime;
+        string[] filesToCopy = Directory.GetFiles(target, "*.*", SearchOption.AllDirectories);
+        nbFilesLeftToDo -= filesToCopy.Where(file => lastChangeDateTime == null || File.GetLastWriteTime(file) > lastChangeDateTime).Count();
+        filesSizeLeftToDo -= filesToCopy.Where(file => lastChangeDateTime == null || File.GetLastWriteTime(file) > lastChangeDateTime).Sum(file => new FileInfo(file).Length);
+        return (nbFilesLeftToDo, filesSizeLeftToDo);
     }
 }
