180c2191ba547dd863977755731e6e93fbebc56c
gitygit362 <axel09.delar@gmail.com>
2025-02-20 16:16:56 +0100

    feature-Extension: changement de l'emplacement de la logique -> settings

--- EasySave/EasySaveGUI/ViewModel/ExecuteSavesViewModel.cs
+++ EasySave/EasySaveGUI/ViewModel/ExecuteSavesViewModel.cs
@@ -15,27 +15,7 @@ namespace EasySaveGUI.ViewModel
 
         public ICommand ExecuteGlobalSaveCommand { get; }
         public ICommand ExecutePartialSaveCommand { get; }
-        public ICommand ChooseExtensionCommand { get; }
 
-        private List<string> _extensions;
-        private string _inputExtensions;
-
-
-        public List<string> Extensions
-        {
-            get => _extensions;
-            set
-            {
-                _extensions = value;
-            }
-        }
-
-
-        public string InputExtensions
-        {
-            get => _inputExtensions;
-            set { _inputExtensions = value; OnPropertyChanged(); }
-        }
 
         public Save SelectedSave
         {
@@ -68,7 +48,6 @@ namespace EasySaveGUI.ViewModel
             _saveRepository = saveRepository;
             ExecuteGlobalSaveCommand = new RelayCommand(_ => ExecuteGlobalSave());
             ExecutePartialSaveCommand = new RelayCommand(_ => ExecutePartialSave());
-            ChooseExtensionCommand = new RelayCommand(_ => ChooseExtension());
         }
 
         private void ExecuteGlobalSave()
@@ -90,25 +69,6 @@ namespace EasySaveGUI.ViewModel
                     MessageBox.Show(errorMessage, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
                 }
             }
-        }
-
-        private void ChooseExtension()
-        {
-            string extensionsEntry = InputExtensions;
-            if (string.IsNullOrWhiteSpace(extensionsEntry))
-            {
-                MessageBox.Show("Veuillez entrer des extensions.");
-                return;
-            }
-
-
-            Extensions = extensionsEntry.Split(';', StringSplitOptions.RemoveEmptyEntries).ToList();
-
-            foreach (var ext in Extensions)
-            {
-                MessageBox.Show(ext);
-            }
-
         }
 
     
