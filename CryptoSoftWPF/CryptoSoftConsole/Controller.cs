3f503f1e9806e5d1bf7561c906753dff1cb5663d
KisaPulga <leoschaeffer03@gmail.com>
2025-02-22 15:02:34 +0100

    Ajout de la mono instance de l'application CryptoSoft

--- CryptoSoftWPF/CryptoSoftConsole/Controller.cs
+++ CryptoSoftWPF/CryptoSoftConsole/Controller.cs
@@ -6,21 +6,30 @@ namespace CryptoConsoleApp
     {
         private readonly ConsoleView _view;
 
+        /// <summary>
+        /// Constructor of the controller
+        /// </summary>
+        /// <param name="view"> The view of the application </param>
         public EncryptionController(ConsoleView view)
         {
             _view = view;
         }
 
+        /// <summary>
+        /// Run the application and display the main menu
+        /// </summary>
         public void Run()
         {
             bool continueRunning = true;
 
             while (continueRunning)
             {
+                // Display the title of the application and get the file path and encryption key
                 _view.DisplayTitle();
                 string filePath = _view.GetFilePath();
                 string encryptKey = _view.GetEncryptionKey();
 
+                // Encrypt the file if the user confirms the operation
                 if (_view.ConfirmEncryption())
                 {
                     _view.ShowMessage("Encryption in progress...");
@@ -32,6 +41,7 @@ namespace CryptoConsoleApp
                     _view.ShowMessage("Operation canceled.");
                 }
 
+                // Ask the user if he wants to encrypt another file or exit the application
                 _view.EncryptAgain();
                 string continueResponse = Console.ReadLine() ?? string.Empty;
 
--- CryptoSoftWPF/CryptoSoftWPF/App.xaml.cs
+++ CryptoSoftWPF/CryptoSoftWPF/App.xaml.cs
@@ -1,14 +1,32 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using System;
+using System.Threading;
 using System.Windows;
 
 namespace CryptoSoftWPF
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
-    }
+        // Mutex to prevent multiple instances of the application 
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            // Mutex to prevent multiple instances of the application
+            bool createdNew;
+            using (Mutex mutex = new Mutex(true, "CryptoSoftWPF_Mutex", out createdNew))
+            {
+                // If the mutex is already created, it means that the application is already running
+                if (!createdNew)
+                {
+                    MessageBox.Show("CryptoSoft is already running!", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    Application.Current.Shutdown();
+                    return;
+                }
 
+                // Start the application if it is not already running
+                base.OnStartup(e);
+
+                // Prevent the mutex from being garbage collected
+                GC.KeepAlive(mutex);
+            }
+        }
+    }
 }
